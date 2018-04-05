ab4122511106bb66ed11d4a9a4727e2d70a93c64
Pep Martinez <pep.martinez@gmail.com>
2018-04-05 18:19:25 +0200

    added a mubsub-backed mongodb capped colelction signaller

---
+++ signal/mongo-capped.js
@@ -0,0 +1,55 @@
+'use strict';
+
+var mubsub = require('mubsub');
+var _ =      require('lodash');
+
+var Signal = require ('../Signal');
+
+class MCSignal extends Signal {
+  constructor (queue, factory, opts) {
+    super (queue, opts);
+    this._factory = factory;
+    
+    this._topic_name = 'keuss:q:signal:' + queue.type () + ':' + queue.name ();
+    this._opts = opts || {};
+    var self = this;
+    
+    this._factory._channel.subscribe (this._topic_name, function (message) {
+      var mature = parseInt (message);
+      
+      // ('got mongo-capped pubsub event on topic [%s], message is %s, calling master.emitInsertion(%d)', self._topic_name, message, mature);
+      self._master.signalInsertion (new Date (mature));
+    });
+  }
+    
+  type () {return MCSignalFactory.Type ()}
+  
+  emitInsertion (mature, cb) { 
+    // ('emit mongo-capped pubsub on topic [%s] mature %d)', this._topic_name, mature);
+    this._factory._channel.publish (this._topic_name, mature.getTime());
+  }
+}
+
+class MCSignalFactory {
+  constructor (opts) {
+    var defaults = {
+      mongo_url: 'mongodb://localhost:27017/keuss_signal',
+      channel: 'default'
+    };
+
+    this._opts = {};
+    _.merge (this._opts, defaults, opts || {});
+    
+    this._mubsub = mubsub (this._opts.mongo_url, this._opts.mongo_opts);
+    this._channel =  this._mubsub.channel (this._opts.channel, this._opts.channel_opts); 
+  }
+
+  static Type () {return 'signal:mongo-capped'}
+  type () {return Type ()}
+
+  signal (channel, opts) {
+    return new MCSignal (channel, this, opts);
+  }
+}
+
+module.exports = MCSignalFactory;
