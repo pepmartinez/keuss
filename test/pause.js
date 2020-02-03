45af7f70601d559861103e089cd65c9fada079f4
Pep Martinez <pep.martinez@gmail.com>
2020-02-03 18:41:03 +0100

    more pause tests

--- test/pause.js
+++ test/pause.js
@@ -1,17 +1,18 @@
 
 var async =  require ('async');
 var should = require ('should');
+var _ =      require ('lodash');
 var MongoClient = require ('mongodb').MongoClient;
 
 var factory = null;
 
 [
-//  {label: 'redis list',          backend: require ('../backends/redis-list')},
-//  {label: 'redis OrderedQueue',  backend: require ('../backends/redis-oq')},
-//  {label: 'pipeline MongoDB',    backend: require ('../backends/pl-mongo')},
-//  {label: 'persistent MongoDB',  backend: require ('../backends/ps-mongo')},
-//  {label: 'plain MongoDB',       backend: require ('../backends/mongo')},
-//  {label: 'MongoDB Bucket',      backend: require ('../backends/bucket-mongo')},
+  {label: 'redis list',          backend: require ('../backends/redis-list')},
+  {label: 'redis OrderedQueue',  backend: require ('../backends/redis-oq')},
+  {label: 'pipeline MongoDB',    backend: require ('../backends/pl-mongo')},
+  {label: 'persistent MongoDB',  backend: require ('../backends/ps-mongo')},
+  {label: 'plain MongoDB',       backend: require ('../backends/mongo')},
+  {label: 'MongoDB Bucket',      backend: require ('../backends/bucket-mongo')},
   {label: 'Safe MongoDB Bucket', backend: require ('../backends/bucket-mongo-safe')},
 ].forEach (backend_item => {
   [
@@ -55,7 +56,7 @@ var factory = null;
         });
 
         it ('queue pauses and resumes ok, existing consumers react accordingly', done => {
-          var q = factory.queue('bench_test_queue_0', {});
+          var q = factory.queue('_test_0_queue_', {});
 
           async.series ([
             cb => q._stats.clear (cb),
@@ -95,7 +96,109 @@ var factory = null;
 
             done ();
           });
-      });
+        });
+
+        it ('pauses ok new consumers if queue paused, resumes them allright', done => {
+          var q = factory.queue('_test_1_queue_', {});
+
+          async.series ([
+            cb => q._stats.clear (cb),
+            cb => {q.pause (true); cb ();},
+
+            cb => async.parallel ([
+              cb => setTimeout (() => q.pop ('me', cb), 100),
+              cb => setTimeout (() => q.push ({q:0, a: 'ryetyeryre 0'}, cb), 1000),
+              cb => setTimeout (() => {q.pause (false); cb ();}, 2000),
+            ], cb),
+
+            cb => setTimeout (cb, 1000),
+
+            cb => q.stats(cb),
+            cb => q.size (cb),
+
+            cb => q._stats.clear (cb),
+          ], (err, res) => {
+            if (err) return done (err);
+            res[4].should.eql ({ put: 1, get: 1 });
+            res[5].should.equal (0);
+            q.nConsumers().should.equal (0);
+
+            done ();
+          });
+        });
+
+        it ('consumer with timeout times out ok if queue was paused', done => {
+          var q = factory.queue('_test_2_queue_', {});
+
+          async.series ([
+            cb => q._stats.clear (cb),
+            cb => {q.pause (true); cb ();},
+
+            cb => async.parallel ([
+              cb => setTimeout (() => q.pop ('me', {timeout: 2000}, err => cb (null, err)), 100),
+              cb => setTimeout (() => q.push ({q:0, a: 'ryetyeryre 0'}, cb), 200),
+            ], cb),
+
+            cb => setTimeout (cb, 1000),
+
+            cb => q.stats((err, res) => {
+              if (err) return cb (err);
+              cb (null, _.cloneDeep (res))
+            }),
+            cb => q.size (cb),
+
+            cb => {q.pause (false); cb ();},
+            cb => q.pop ('me', cb),
+
+            cb => q._stats.clear (cb)
+          ], (err, res) => {
+            if (err) return done (err);
+            res[4].should.eql ({ put: 1 });
+            res[5].should.equal (1);
+            res[2][0].timeout.should.eql (true);
+            res[7].payload.should.eql ({q:0, a: 'ryetyeryre 0'});
+
+            q.nConsumers().should.equal (0);
+
+            done ();
+          });
+        });
+
+        it ('consumer with timeout times out ok if queue is paused while waiting for delayed item', done => {
+          var q = factory.queue('_test_3_queue_', {});
+
+          async.series ([
+            cb => q._stats.clear (cb),
+            cb => q.push ({q:0, a: 'ryetyeryre 0'}, {delay: 3}, cb),
+            cb => {q.pause (true); cb ();},
+            cb => setTimeout (cb, 100),
+
+            cb => q.pop ('me', {timeout: 1000}, err => cb (null, err)),
+
+            cb => setTimeout (cb, 1000),
+
+            cb => q.stats((err, res) => {
+              if (err) return cb (err);
+              cb (null, _.cloneDeep (res))
+            }),
+            cb => q.totalSize (cb),
+
+            cb => {q.pause (false); cb ();},
+            cb => q.pop ('me', cb),
+
+            cb => q._stats.clear (cb)
+          ], (err, res) => {
+            if (err) return done (err);
+            res[6].should.eql ({ put: 1 });
+            res[7].should.equal (1);
+            res[4].timeout.should.eql (true);
+            res[9].payload.should.eql ({q:0, a: 'ryetyeryre 0'});
+
+            q.nConsumers().should.equal (0);
+
+            done ();
+          });
+        });
 
       });
     });
