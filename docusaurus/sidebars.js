2df6a8db53968a8a5c60d0ab2b7fc3c686ef774d
Pep Martinez <pep.martinez@gmail.com>
2020-09-09 20:04:22 +0200

    docusaurus: added usage/putting-all-together.md

--- docusaurus/sidebars.js
+++ docusaurus/sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   someSidebar: {
     Intro:     ['about', 'quickstart', 'concepts'],
-    Usage:     ['usage/buckets', 'usage/shutdown', 'usage/no-signaller', 'usage/redis-conns', 'usage/pipelines'],
+    Usage:     ['usage/putting-all-together', 'usage/buckets', 'usage/shutdown', 'usage/no-signaller', 'usage/redis-conns', 'usage/pipelines'],
     API:       ['api/factory', 'api/signal', 'api/stats', 'api/queue'],
     Examples:  ['examples'],
     Changelog: ['changelog'],
