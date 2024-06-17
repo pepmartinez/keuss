63fcb3f3fc6cb68f4ef332fefa9da3305a0d4d9f
pepmartinez <pep.martinez@gmail.com>
2024-06-17 21:03:41 +0200

    added more tests to queue() async

--- test/pipeline-Sink.js
+++ test/pipeline-Sink.js
@@ -1,23 +1,31 @@
 
-var async =   require ('async');
-var should =  require ('should');
+const async =   require ('async');
+const should =  require ('should');
 
-var LocalSignal = require ('../signal/local');
-var MemStats =    require ('../stats/mem');
+const LocalSignal = require ('../signal/local');
+const MemStats =    require ('../stats/mem');
 
-var PDL = require ('../Pipeline/DirectLink');
-var PS  = require ('../Pipeline/Sink');
+const PDL = require ('../Pipeline/DirectLink');
+const PS  = require ('../Pipeline/Sink');
 
 const MongoClient = require ('mongodb').MongoClient;
 
-var factory = null;
+
+process.on('unhandledRejection', (err, p) => {
+  console.error('unhandledRejection', err.stack, p)
+});
+
+let factory = null;
+
 
 [
   {label: 'Pipelined MongoDB',  mq: require ('../backends/pl-mongo')}
-].forEach (function (MQ_item) {
-  describe ('Pipeline/Sink operations over ' + MQ_item.label, function () {
-    var MQ = MQ_item.mq;
+].forEach (MQ_item => {
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  describe ('Pipeline/Sink operations over ' + MQ_item.label, () => {
+    const MQ = MQ_item.mq;
 
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     before (done => {
       var opts = {
         url: 'mongodb://localhost/__test_pipeline_sink__',
@@ -33,6 +41,8 @@ var factory = null;
       });
     });
 
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     after (done => async.series ([
       cb => setTimeout (cb, 1000),
       cb => factory.close (cb),
@@ -43,132 +53,144 @@ var factory = null;
     ], done));
 
 
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     it ('3-elem pipeline flows begin to end, sink at the end', done => {
-      var q_opts = {};
-      var q1 = factory.queue ('test_1_pl_1', q_opts);
-      var q2 = factory.queue ('test_1_pl_2', q_opts);
-      var q3 = factory.queue ('test_1_pl_3', q_opts);
+      const q_opts = {};
 
-      // tie them up, q1 -> q2 -> q3 -> sink
-      var pll1 = new PDL (q1, q2);
-      var pll2 = new PDL (q2, q3);
-      var snk1 = new PS  (q3);
+      async.parallel ({
+        q1: cb => factory.queue ('test_1_pl_1', q_opts, cb),
+        q2: cb => factory.queue ('test_1_pl_2', q_opts, cb),
+        q3: cb => factory.queue ('test_1_pl_3', q_opts, cb),
+      }, (err, qs) => {
+        if (err) return done(err);
+            
+        // tie them up, q1 -> q2 -> q3 -> sink
+        const pll1 = new PDL (qs.q1, qs.q2);
+        const pll2 = new PDL (qs.q2, qs.q3);
+        const snk1 = new PS  (qs.q3);
 
-      pll1.start ((elem, done0) => {
-        elem.should.match ({
-          payload: { a: 5, b: 'see it run...' },
-          tries: 0,
-          _q: 'test_1_pl_1'
-        });
-
-        done0();
-      });
-
-      pll2.start ((elem, done0) => {
-        elem.should.match ({
-          payload: { a: 5, b: 'see it run...' },
-          tries: 0,
-          _q: 'test_1_pl_2'
-        });
-
-        done0();
-      });
-
-      snk1.start ((elem, done0) => {
-        elem.should.match ({
-          payload: { a: 5, b: 'see it run...' },
-          tries: 0,
-          _q: 'test_1_pl_3'
-        });
-
-        done0();
-
-        setTimeout (() => {
-          pll1.stop();
-          pll2.stop();
-          snk1.stop();
-
-          async.series ([
-            cb => q1.totalSize(cb),
-            cb => q2.totalSize(cb),
-            cb => q3.totalSize(cb)
-          ], (err, res) => {
-            if (err) return done (err);
-            res.should.eql ([0,0,0]);
-            done ();
+        pll1.start ((elem, done0) => {
+          elem.should.match ({
+            payload: { a: 5, b: 'see it run...' },
+            tries: 0,
+            _q: 'test_1_pl_1'
           });
-        }, 500);
-      });
 
-      q1.push ({a:5, b:'see it run...'}, {}, () => {});
+          done0();
+        });
+
+        pll2.start ((elem, done0) => {
+          elem.should.match ({
+            payload: { a: 5, b: 'see it run...' },
+            tries: 0,
+            _q: 'test_1_pl_2'
+          });
+
+          done0();
+        });
+
+        snk1.start ((elem, done0) => {
+          elem.should.match ({
+            payload: { a: 5, b: 'see it run...' },
+            tries: 0,
+            _q: 'test_1_pl_3'
+          });
+
+          done0();
+
+          setTimeout (() => {
+            pll1.stop();
+            pll2.stop();
+            snk1.stop();
+
+            async.series ([
+              cb => qs.q1.totalSize(cb),
+              cb => qs.q2.totalSize(cb),
+              cb => qs.q3.totalSize(cb)
+            ], (err, res) => {
+              if (err) return done (err);
+              res.should.eql ([0,0,0]);
+              done ();
+            });
+          }, 500);
+        });
+
+        qs.q1.push ({a:5, b:'see it run...'}, {}, () => {});
+      });
     });
 
 
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     it ('3-elem pipeline flows begin to end, sink at the end, with sink retries', done => {
-      var q_opts = {};
-      var q1 = factory.queue ('test_1_pl_1', q_opts);
-      var q2 = factory.queue ('test_1_pl_2', q_opts);
-      var q3 = factory.queue ('test_1_pl_3', q_opts);
+      const q_opts = {};
 
-      // tie them up, q1 -> q2 -> q3 -> sink
-      var pll1 = new PDL (q1, q2);
-      var pll2 = new PDL (q2, q3);
-      var snk1 = new PS  (q3);
+      async.parallel ({
+        q1: cb => factory.queue ('test_1_pl_1', q_opts, cb),
+        q2: cb => factory.queue ('test_1_pl_2', q_opts, cb),
+        q3: cb => factory.queue ('test_1_pl_3', q_opts, cb),
+      }, (err, qs) => {
+        if (err) return done(err);
 
-      var try_count = 0;
+        // tie them up, q1 -> q2 -> q3 -> sink
+        const pll1 = new PDL (qs.q1, qs.q2);
+        const pll2 = new PDL (qs.q2, qs.q3);
+        const snk1 = new PS  (qs.q3);
 
-      pll1.start ((elem, done0) => {
-        elem.should.match ({
-          payload: { a: 5, b: 'see it run...' },
-          tries: 0,
-          _q: 'test_1_pl_1'
-        });
+        let try_count = 0;
 
-        done0();
-      });
-
-      pll2.start ((elem, done0) => {
-        elem.should.match ({
-          payload: { a: 5, b: 'see it run...' },
-          tries: 0,
-          _q: 'test_1_pl_2'
-        });
-
-        done0();
-      });
-
-      snk1.start ((elem, done0) => {
-        if (try_count < 2) {
-          try_count++;
-          return done0 ({e: 'error, retry'});
-        }
-
-        elem.should.match ({
-          payload: { a: 5, b: 'see it run...' },
-          tries: 2,
-          _q: 'test_1_pl_3'
-        });
-
-        done0();
-
-        setTimeout (() => {
-          pll1.stop();
-          pll2.stop();
-          snk1.stop();
-
-          async.series ([
-            cb => q1.totalSize(cb),
-            cb => q2.totalSize(cb),
-            cb => q3.totalSize(cb)
-          ], (err, res) => {
-            if (err) return done (err);
-            res.should.eql ([0,0,0]);
-            done ();
+        pll1.start ((elem, done0) => {
+          elem.should.match ({
+            payload: { a: 5, b: 'see it run...' },
+            tries: 0,
+            _q: 'test_1_pl_1'
           });
-        }, 500);
-      });
 
-      q1.push ({a:5, b:'see it run...'}, {}, () => {});
+          done0();
+        });
+
+        pll2.start ((elem, done0) => {
+          elem.should.match ({
+            payload: { a: 5, b: 'see it run...' },
+            tries: 0,
+            _q: 'test_1_pl_2'
+          });
+
+          done0();
+        });
+
+        snk1.start ((elem, done0) => {
+          if (try_count < 2) {
+            try_count++;
+            return done0 ({e: 'error, retry'});
+          }
+
+          elem.should.match ({
+            payload: { a: 5, b: 'see it run...' },
+            tries: 2,
+            _q: 'test_1_pl_3'
+          });
+
+          done0();
+
+          setTimeout (() => {
+            pll1.stop();
+            pll2.stop();
+            snk1.stop();
+
+            async.series ([
+              cb => qs.q1.totalSize(cb),
+              cb => qs.q2.totalSize(cb),
+              cb => qs.q3.totalSize(cb)
+            ], (err, res) => {
+              if (err) return done (err);
+              res.should.eql ([0,0,0]);
+              done ();
+            });
+          }, 500);
+        });
+
+        qs.q1.push ({a:5, b:'see it run...'}, {}, () => {});
+      });
     });
 
   });
